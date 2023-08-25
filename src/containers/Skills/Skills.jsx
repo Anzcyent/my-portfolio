d8bbf3c5c48213e145c2094cc74820e8e3f47873
Cihan <ozcancihan714@gmail.com>
2023-08-25 17:18:07 +0300

    Skills design update

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -7,13 +7,13 @@ const Skills = () => {
     <section className="min-h-[100vh] font-poppins p-3 bg-darkSecondary text-white overflow-auto custom-scroll">
       <div className="wrapper flex lg:flex-row flex-col justify-center w-full h-full lg:gap-80 gap-0">
         <div className="lg:w-1/2 w-full grid grid-cols-1">
-          <h3 className="font-bold text-gradient-blue lg:text-4xl text-2xl mb-7 lg:mt-0 mt-10">
+          <h3 className="font-bold text-gradient-blue lg:text-4xl text-2xl mb-7 lg:mt-0 mt-20">
             Language/Framework Skills
           </h3>
           {skills.map((skill, i) => (
             <div
               key={skill.name + i}
-              className="flex items-center w-full mt-10 hover-and-scale"
+              className="flex items-center w-full lg:mt-10 mt-5 hover-and-scale"
             >
               <img
                 src={skill.image}
