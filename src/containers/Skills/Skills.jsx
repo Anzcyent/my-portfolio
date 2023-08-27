3e859d0b067818d4b6d2dd47635f6126b8c0b5da
Cihan <ozcancihan714@gmail.com>
2023-08-27 15:01:43 +0300

    Skills design change

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Skills.css"
 import ProgressBar from "@ramonak/react-progress-bar";
 import { skills, personalSkills, developerSkills } from "../../utils/data";
 import { SkillInfo } from "../../components";
@@ -20,7 +21,7 @@ const Skills = () => {
     <section className="min-h-[100vh] font-poppins p-3 bg-darkSecondary text-white overflow-auto custom-scroll">
       <div className="wrapper flex lg:flex-row flex-col justify-center w-full h-full lg:gap-80 gap-0">
         <div className="lg:w-1/2 w-full grid grid-cols-1">
-          <h3 className="font-bold text-gradient-blue lg:text-4xl text-2xl mb-7 lg:mt-0 mt-20">
+          <h3 className="skill-heading font-bold text-gradient-blue lg:text-4xl text-2xl mb-7 lg:mt-0 mt-20">
             Language Skills
           </h3>
           {sortedSkills.map((skill, i) => (
@@ -54,7 +55,7 @@ const Skills = () => {
 
         <div className="grid grid-rows-1 gap-4 lg:w-1/2 w-full h-full">
           <div className="h-1/2">
-            <h3 className="font-bold text-gradient-blue lg:text-4xl text-2xl mb-7 lg:mt-0 mt-20">
+            <h3 className="skill-heading font-bold text-gradient-blue lg:text-4xl text-2xl mb-7 lg:mt-0 mt-20">
               Personal Skills
             </h3>
 
@@ -88,7 +89,7 @@ const Skills = () => {
           </div>
 
           <div className="h-1/2">
-            <h3 className="font-bold text-gradient-blue lg:text-4xl text-2xl mb-7 mt-20">
+            <h3 className="skill-heading font-bold text-gradient-blue lg:text-4xl text-2xl mb-7 mt-20">
               Developer Skills
             </h3>
 
