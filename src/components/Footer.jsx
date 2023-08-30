cf12b1140dffbaa05b8d6228821da2487a2171a1
Cihan <ozcancihan714@gmail.com>
2023-08-30 16:28:40 +0300

    Footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { AiFillPhone, AiFillMail } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
-}
+    <footer className="bg-dark text-white font-poppins p-3">
+      <div className="wrapper flex justify-evenly">
+        <h5>Cihan Özcan</h5>
 
-export default Footer
+        <small className="flex items-center justify-center gap-1">
+          <AiFillPhone />
+          +90 546 717 74 21
+        </small>
+
+        <small className="flex items-center justify-center gap-1">
+          <AiFillMail />
+          ozcancihan714@gmail.com
+        </small>
+
+        <small className="font-bold">ANKARA</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/containers/CV/CV.jsx
+++ src/containers/CV/CV.jsx
@@ -4,7 +4,7 @@ import "./CV.css";
 const CV = () => {
   return (
     <section className="bg-darkSecondary min-h-[100vh] text-white p-3 font-poppins">
-      <div className="wrapper md:mt-0 mt-20">
+      <div className="wrapper md:mt-10 mt-20">
         <div className="cluster">
           <div className="grape"></div>
           <div className="grape"></div>
@@ -17,7 +17,7 @@ const CV = () => {
             <span>You can access my resumé by clicking the button below.</span>
             <a
               href="/downloads/dummyCV.txt"
-              className="bg-gradient p-3 hover-and-scale my-20"
+              className="bg-gradient p-3 hover-and-scale my-10"
               download
             >
               DOWNLOAD CV
