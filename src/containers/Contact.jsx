d31eadf5b0b400d29e40edea7a64890a25566653
Cihan <ozcancihan714@gmail.com>
2023-08-31 12:44:05 +0300

    Contact with emailjs done

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,15 +1,70 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_m5hry56",
+        "template_hlurdc3",
+        form.current,
+        "IfFV-JnDdjx2uaF-5"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+
   return (
     <section className="min-h-[100vh] bg-darkSecondary text-white font-poppins p-3">
       <div className="wrapper mt-20">
-        <h4 className="text-4xl font-bold text-gradient-purple text-center my-10">Contact Me</h4>
-        <form className="flex flex-col md:px-40 px-0">
-          <input type="text" className="w-full p-2 border-2 outline-0 my-5" placeholder="Your name" style={{background: "transparent"}} />
-          <textarea type="text" className="w-full p-2 border-2 outline-0 my-5" placeholder="Your message" rows={10} style={{background: "transparent"}} />
+        <h4 className="text-4xl font-bold text-gradient-purple text-center my-10">
+          Contact Me
+        </h4>
+        <form ref={form} onSubmit={sendEmail} className="flex flex-col md:px-40 px-0">
+          <input
+            type="text"
+            className="w-full p-2 border-2 outline-0 my-5"
+            placeholder="Your name"
+            style={{ background: "transparent" }}
+            required
+            name="user_name"
+          />
+          <input
+            type="email"
+            className="w-full p-2 border-2 outline-0 my-5"
+            placeholder="Your email address"
+            style={{ background: "transparent" }}
+            required
+            name="user_email"
+          />
+          <textarea
+            type="text"
+            className="w-full p-2 border-2 outline-0 my-5"
+            placeholder="Your message"
+            rows={10}
+            style={{ background: "transparent" }}
+            required
+            name="message"
+          />
 
-          <button className="bg-gradient p-3 hover-and-scale self-center w-full my-5">Send</button>
+          <button
+            type="submit"
+            className="bg-dark p-3 hover-and-scale self-center w-full my-5"
+          >
+            Send
+          </button>
         </form>
       </div>
     </section>
